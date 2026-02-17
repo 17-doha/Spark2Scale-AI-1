9da709ee29a6c6135886604c23ea5d0b710a3bae
Doha <doha.hemdan7@gmail.com>
2026-02-18 00:25:20 +0200

    addded pdf extractor

--- app/api/main.py
+++ app/api/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.routes import ppt_generation, evaluation, market_research, recommendation
+from app.api.routes import ppt_generation, evaluation, market_research, recommendation, pdf_extraction
 import uvicorn
 
 app = FastAPI(title="Spark2Scale AI Agent")
@@ -10,6 +10,8 @@ app.include_router(evaluation.router, prefix="/api/v1/evaluation", tags=["Evalua
 app.include_router(recommendation.router, prefix="/api/v1", tags=["Recommendation"])
 
 app.include_router(market_research.router, prefix="/api/v1/market-research", tags=["Market Research"])
+
+app.include_router(pdf_extraction.router, prefix="/api/v1/pdf", tags=["PDF Extraction"])
 
 
 @app.get("/")
