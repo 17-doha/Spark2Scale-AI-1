3da7a48203f56153b4c2c8789d49da236165147f
Salma Ali <202200622@zewailcity.edu.eg>
2026-05-25 15:56:28 +0300

    bug: Don't recommend  the pitches the usered aready seen before

--- app/graph/feed_recommedation_agent/state.py
+++ app/graph/feed_recommedation_agent/state.py
@@ -1,18 +1,15 @@
-"""
-app/graph/feed_recommedation_agent/state.py
-"""
-
 from typing import Optional, Annotated
 import operator
 from typing_extensions import TypedDict
 
 
 class FilteredSearchState(TypedDict):
-    investor_id    : str
-    filter_tags    : list[str]
-    investor_vector: Optional[list[float]]
-    candidates     : list[dict]
-    final_results  : list[dict]
-    errors         : Annotated[list[str], operator.add]
-    fallback_triggered : bool
-    sibling_tags       : list[str]
+    investor_id         : str
+    filter_tags         : list[str]
+    investor_vector     : Optional[list[float]]
+    candidates          : list[dict]
+    final_results       : list[dict]
+    errors              : Annotated[list[str], operator.add]
+    fallback_triggered  : bool
+    sibling_tags        : list[str]
+    seen_pitchdeck_ids  : list[str]
